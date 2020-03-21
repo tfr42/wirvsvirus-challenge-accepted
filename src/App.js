6dce77d77af191cd3f61c833f9cffdf787ee963f
Torsten Friebe <torsten.friebe@gmail.com>
2020-03-22 00:32:34 +0100

    Logo added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Logo from "./wirvsviruslogo.png"
 import "./styles.css";
 
 export default function App() {
@@ -6,6 +7,7 @@ export default function App() {
     <div className="App">
       <h1>Hello, Challenge Accepted!</h1>
       <h2>Some magic things will happen soon!</h2>
+      <img src={Logo} alt="wirvsvirus logo"/>
     </div>
   );
 }
